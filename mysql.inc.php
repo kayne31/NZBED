bb851b270a5ee6d5575226f9add1b5542e1e5644
NZBED <2pokereyes@gmail.com>
2012-08-02 00:42:36 +0200

    Small Changes

--- mysql.inc.php
+++ mysql.inc.php
@@ -3,9 +3,9 @@
 if (!defined('MYSQL_INCLUDE')) { require_once( INCLUDEPATH .'mysql.php' ); }
 
 $options['hostname'] = 'localhost'; // Hostname
-$options['username'] = 'nzbed'; // Username
-$options['password'] = 'erika98'; // Password
-$options['dbname'] = 'nzbed'; // Database name
+$options['username'] = ''; // Username
+$options['password'] = ''; // Password
+$options['dbname'] = ''; // Database name
 
 $db = new mysql( $options );
 $db->connect();
