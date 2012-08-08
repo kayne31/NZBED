c34d465129f9a4c28d4456fa65714151e98d526a
NZBED <2pokereyes@gmail.com>
2012-08-08 11:31:38 +0200

    Removed all drop statements and cleaned it up

--- nzbed.sql
+++ nzbed.sql
@@ -1,30 +1,14 @@
 CREATE DATABASE  IF NOT EXISTS `nzbed` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `nzbed`;
--- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
---
--- Host: localhost    Database: nzbed
--- ------------------------------------------------------
--- Server version	5.5.25
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `tvrage_show`
 --
 
-DROP TABLE IF EXISTS `tvrage_show`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tvrage_show` (
+CREATE TABLE IF NOT EXISTS `tvrage_show` (
   `showID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tvrageShowID` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -40,10 +24,10 @@ CREATE TABLE `tvrage_show` (
 -- Table structure for table `mbrainz_album`
 --
 
-DROP TABLE IF EXISTS `mbrainz_album`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mbrainz_album` (
+CREATE TABLE IF NOT EXISTS `mbrainz_album` (
   `albumID` varchar(255) NOT NULL DEFAULT '',
   `artist` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -58,10 +42,10 @@ CREATE TABLE `mbrainz_album` (
 -- Table structure for table `rovi_album`
 --
 
-DROP TABLE IF EXISTS `rovi_album`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rovi_album` (
+CREATE TABLE IF NOT EXISTS `rovi_album` (
   `albumID` varchar(255) NOT NULL DEFAULT '',
   `artist` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -76,10 +60,9 @@ CREATE TABLE `rovi_album` (
 -- Table structure for table `rovi_search`
 --
 
-DROP TABLE IF EXISTS `rovi_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rovi_search` (
+
+
+CREATE TABLE IF NOT EXISTS `rovi_search` (
   `search` varchar(255) NOT NULL,
   `albumID` varchar(255) NOT NULL,
   PRIMARY KEY (`search`)
@@ -90,10 +73,8 @@ CREATE TABLE `rovi_search` (
 -- Table structure for table `anidb_search`
 --
 
-DROP TABLE IF EXISTS `anidb_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `anidb_search` (
+
+CREATE TABLE IF NOT EXISTS `anidb_search` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `search` varchar(255) NOT NULL DEFAULT '',
   `anidbID` varchar(255) NOT NULL DEFAULT '',
@@ -107,10 +88,8 @@ CREATE TABLE `anidb_search` (
 -- Table structure for table `gamespot_search`
 --
 
-DROP TABLE IF EXISTS `gamespot_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gamespot_search` (
+
+CREATE TABLE IF NOT EXISTS `gamespot_search` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `search` varchar(255) NOT NULL DEFAULT '',
   `gsUrl` varchar(255) NOT NULL DEFAULT '',
@@ -122,10 +101,8 @@ CREATE TABLE `gamespot_search` (
 -- Table structure for table `discogs_album`
 --
 
-DROP TABLE IF EXISTS `discogs_album`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `discogs_album` (
+
+CREATE TABLE IF NOT EXISTS `discogs_album` (
   `albumID` varchar(255) NOT NULL DEFAULT '',
   `artist` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -141,10 +118,8 @@ CREATE TABLE `discogs_album` (
 -- Table structure for table `imdb_film`
 --
 
-DROP TABLE IF EXISTS `imdb_film`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `imdb_film` (
+
+CREATE TABLE IF NOT EXISTS `imdb_film` (
   `filmID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `imdbID` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -161,10 +136,8 @@ CREATE TABLE `imdb_film` (
 -- Table structure for table `anidb_anime`
 --
 
-DROP TABLE IF EXISTS `anidb_anime`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `anidb_anime` (
+
+CREATE TABLE IF NOT EXISTS `anidb_anime` (
   `animeID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `anidbID` varchar(255) NOT NULL DEFAULT '',
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -180,10 +153,8 @@ CREATE TABLE `anidb_anime` (
 -- Table structure for table `tmdb_search`
 --
 
-DROP TABLE IF EXISTS `tmdb_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tmdb_search` (
+
+CREATE TABLE IF NOT EXISTS `tmdb_search` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `search` varchar(255) NOT NULL DEFAULT '',
   `tmdbID` varchar(255) NOT NULL DEFAULT '',
@@ -195,10 +166,8 @@ CREATE TABLE `tmdb_search` (
 -- Table structure for table `gamespot_game`
 --
 
-DROP TABLE IF EXISTS `gamespot_game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gamespot_game` (
+
+CREATE TABLE IF NOT EXISTS `gamespot_game` (
   `gsID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `gsUrl` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -208,16 +177,14 @@ CREATE TABLE `gamespot_game` (
   PRIMARY KEY (`gsID`),
   KEY `gsUrl` (`gsUrl`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `tmdb_film`
 --
 
-DROP TABLE IF EXISTS `tmdb_film`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tmdb_film` (
+
+CREATE TABLE IF NOT EXISTS `tmdb_film` (
   `filmID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tmdbID` varchar(255) NOT NULL DEFAULT '',
   `title` varchar(255) NOT NULL DEFAULT '',
@@ -227,32 +194,28 @@ CREATE TABLE `tmdb_film` (
   `aka` varchar(255) NOT NULL,
   PRIMARY KEY (`filmID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `imdb_search`
 --
 
-DROP TABLE IF EXISTS `imdb_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `imdb_search` (
+
+CREATE TABLE IF NOT EXISTS `imdb_search` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `search` varchar(255) NOT NULL DEFAULT '',
   `imdbID` varchar(255) NOT NULL DEFAULT '',
   `fimdbID` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `tvrage_search`
 --
 
-DROP TABLE IF EXISTS `tvrage_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tvrage_search` (
+
+CREATE TABLE IF NOT EXISTS `tvrage_search` (
   `searchID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `search` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `tvrageShowID` int(11) NOT NULL,
@@ -260,30 +223,26 @@ CREATE TABLE `tvrage_search` (
   PRIMARY KEY (`searchID`),
   KEY `search` (`search`)
 ) ENGINE=MyISAM AUTO_INCREMENT=158 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `mbrainz_search`
 --
 
-DROP TABLE IF EXISTS `mbrainz_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mbrainz_search` (
+
+CREATE TABLE IF NOT EXISTS `mbrainz_search` (
   `search` varchar(255) NOT NULL,
   `albumID` varchar(255) NOT NULL,
   PRIMARY KEY (`search`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `tvrage_episode`
 --
 
-DROP TABLE IF EXISTS `tvrage_episode`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tvrage_episode` (
+
+CREATE TABLE IF NOT EXISTS `tvrage_episode` (
   `episodeID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tvrageEpisodeID` int(10) unsigned NOT NULL DEFAULT '0',
   `tvrageShowID` int(11) NOT NULL,
@@ -296,16 +255,14 @@ CREATE TABLE `tvrage_episode` (
   KEY `tvrageEpisodeID` (`tvrageEpisodeID`),
   KEY `tvrageShowID` (`tvrageShowID`,`series`,`episode`)
 ) ENGINE=MyISAM AUTO_INCREMENT=764 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `query_fail`
 --
 
-DROP TABLE IF EXISTS `query_fail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `query_fail` (
+
+CREATE TABLE IF NOT EXISTS `query_fail` (
   `queryID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `query` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -315,30 +272,17 @@ CREATE TABLE `query_fail` (
   `status` enum('IGNORE','OPEN','FIXED') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'OPEN',
   PRIMARY KEY (`queryID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `discogs_search`
 --
 
-DROP TABLE IF EXISTS `discogs_search`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `discogs_search` (
+
+CREATE TABLE IF NOT EXISTS `discogs_search` (
   `search` varchar(255) NOT NULL,
   `albumID` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   PRIMARY KEY (`search`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-08-07 22:44:52
